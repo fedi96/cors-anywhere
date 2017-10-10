2bfcefc22426e435cc631302627cf2f8d3ea3a72
fedi96 <fedi96@users.noreply.github.com>
2017-10-11 01:51:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['' ,'http://www.jokerstream.com' ,'http://rjh217.herokuapp.com' ,'http://www.rjh217.win/'],//originWhitelist,// 
+  originWhitelist: originWhitelist,//['' ,'http://www.jokerstream.com' ,'http://rjh217.herokuapp.com' ,'http://www.rjh217.win/'],// 
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
